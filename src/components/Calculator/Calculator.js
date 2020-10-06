daedc5ffb4779d3237f0af71db3dc33d3fac1cd9
Joel Roske <joel.j.roske@gmail.com>
2020-10-06 17:19:06 -0500

    buttons display and click register

--- src/components/Calculator/Calculator.js
+++ src/components/Calculator/Calculator.js
@@ -1,9 +1,47 @@
 import React, { Component } from "react";
 
-
 class Calculator extends Component {
-    
+  state = {
+    displayValue: "0",
+    operator: null,
+    firstVal: "",
+    secondVal: "",
+  };
 
-    
+  handleClick = (index) => {
+    console.log("clicked", index);
+  };
+
+  render() {
+    const buttons = [
+      "CLR",
+      "DEL",
+      "7",
+      "8",
+      "9",
+      "/",
+      "4",
+      "5",
+      "6",
+      "x",
+      "1",
+      "2",
+      "3",
+      "-",
+      "0",
+      ".",
+      "=",
+      "+",
+    ];
+    return (
+      <div>
+        {buttons.map((button, index) => (
+          <button key={index} onClick={()=>this.handleClick(button)}>
+            {button}
+          </button>
+        ))}
+      </div>
+    );
+  }
 }
 export default Calculator;
